745c691989c2a02d4f945eb1920c081671fdf8e6
ved4a <vedaas.anand@gmail.com>
2024-03-22 16:25:20 +0530

    feat: Add static text and icons

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -7,7 +7,7 @@ const CurrentWeather = () => {
         <p className="city">Bengaluru</p>
         <p className="weather-description">Sunny</p>
       </div>
-      <img alt="weather" className="weather-icon"></img>
+      <img alt="weather" className="weather-icon" src="icons/01d.svg" />
     </div>
   );
 };
