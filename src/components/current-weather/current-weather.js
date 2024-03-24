501d6fb6e59215ce95df863e1ae7a8b1827d4cf8
ved4a <vedaas.anand@gmail.com>
2024-03-24 17:42:45 +0530

    feat: Create partial skeleton for current weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -11,7 +11,16 @@ const CurrentWeather = () => {
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
       <div className="bottom">
-        <p className="temperature"></p>
+        <p className="temperature">25°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+        </div>
       </div>
     </div>
   );
