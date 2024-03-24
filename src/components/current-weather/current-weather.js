06d98ecf5c61bf503950cf75cb885b39e84a15af
ved4a <vedaas.anand@gmail.com>
2024-03-24 19:10:50 +0530

    feat: Create static current weather component and also almost-complete design

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -14,11 +14,23 @@ const CurrentWeather = () => {
         <p className="temperature">25°C</p>
         <div className="details">
           <div className="parameter-row">
-            <span className="parameter-label">Details</span>
+            <span className="parameter-label top">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
             <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">12 hPa</span>
           </div>
         </div>
       </div>
