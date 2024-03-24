05fb62883ef8aa02f10d1bcadbd2863d38736f23
ved4a <vedaas.anand@gmail.com>
2024-03-24 19:26:32 +0530

    feat: Fetch data using APIs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import { OPENWEATHER_API_KEY, OPENWEATHER_API_URL } from "./api";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(``);
+    const currentWeatherFetch = fetch(
+      `${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`
+    );
+
+    const forecastFetch = fetch(
+      `${OPENWEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[0].json();
+    });
   };
 
   return (
